586bbcc5f15d210f6c5691b0757a87690afe18ef
Mike Golubitsky <mike617@gmail.com>
2015-02-24 11:14:42 -0800

    remove authenticity token verification

--- app/controllers/api/games_controller.rb
+++ app/controllers/api/games_controller.rb
@@ -1,24 +1,26 @@
 class Api::GamesController < ApplicationController
 
+  skip_before_filter :verify_authenticity_token
+
   def index
     @games = Game.high_scores
     render 'api/games/index'
   end
 
   def create
-    @game = current_user.photos.new(photo_params)
+    @game = Game.new(game_params)
 
     if @game.save
       @games = Game.high_scores
       render json: @game
     else
-      render json: { error: "There was an internal error." }, status: :unprocessable_entity
+      render json: { error: @game.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
   private
 
   def game_params
-    params.require(:game).permit(:name, :score, :lines, :start_level, :end_level, :time)
+    params.require(:game).permit(:username, :score, :lines, :start_level, :end_level, :time)
   end
 end
