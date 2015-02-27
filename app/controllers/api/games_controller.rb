b9df6d4a1294b313aad046f9f53d992a048ea855
Mike Golubitsky <mike617@gmail.com>
2015-02-27 02:23:55 -0800

    try manual limitation

--- app/controllers/api/games_controller.rb
+++ app/controllers/api/games_controller.rb
@@ -2,12 +2,25 @@ class Api::GamesController < ApplicationController
 
   skip_before_filter :verify_authenticity_token
 
+
   def index
+    puts "index action"
+    unless request.headers['origin'] == 'http://golubitsky.github.io'
+      render json: { error: "Unapproved origin." }
+      return
+    end
+
     @games = Game.high_scores
     render 'api/games/index'
   end
 
   def create
+    puts "create action"
+    unless request.headers['origin'] == 'http://golubitsky.github.io'
+      render json: { error: "Unapproved origin." }
+      return
+    end
+
     @game = Game.new(game_params)
     @game.username = @game.username.upcase
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery
-  after_filter :cors_set_access_control_headers
+  before_filter :cors_set_access_control_headers
 
   # For all responses in this controller, return the CORS access control headers.
 
   def cors_set_access_control_headers
+    puts "application controller"
+    puts request
     headers['Access-Control-Allow-Origin'] = 'http://golubitsky.github.io'
     headers['Access-Control-Allow-Methods'] = 'POST, GET'
     # headers['Access-Control-Allow-Headers'] = '*'
