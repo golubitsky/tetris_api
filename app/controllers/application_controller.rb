0a068f538b8515ddc6abffc86aaeba50d164caa8
Mike Golubitsky <mike617@gmail.com>
2015-02-27 01:55:39 -0800

    fix typo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,9 +9,9 @@ class ApplicationController < ActionController::Base
   def cors_set_access_control_headers
     headers['Access-Control-Allow-Origin'] = 'http://golubitsky.github.io'
     headers['Access-Control-Allow-Methods'] = 'POST, GET'
-    headers['Access-Control-Allow-Headers'] = '*'
-    headers['Access-Control-Request-Method'] = '*'
-    headers['Access-Control-Max-Age'] = "1728000"
-  # end
+    # headers['Access-Control-Allow-Headers'] = '*'
+    # headers['Access-Control-Request-Method'] = '*'
+    # headers['Access-Control-Max-Age'] = "1728000"
+  end
 
 end
