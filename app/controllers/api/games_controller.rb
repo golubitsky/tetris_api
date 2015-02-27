4af108abc4469691713691f36401f6f41feab71a
Mike Golubitsky <mike617@gmail.com>
2015-02-27 02:38:07 -0800

    clean up API changes

--- app/controllers/api/games_controller.rb
+++ app/controllers/api/games_controller.rb
@@ -2,25 +2,14 @@ class Api::GamesController < ApplicationController
 
   skip_before_filter :verify_authenticity_token
 
+  before_action :approved_origin?
 
   def index
-    puts "index action"
-    unless request.headers['origin'] == 'http://golubitsky.github.io'
-      render json: { error: "Unapproved origin." }
-      return
-    end
-
     @games = Game.high_scores
     render 'api/games/index'
   end
 
   def create
-    puts "create action"
-    unless request.headers['origin'] == 'http://golubitsky.github.io'
-      render json: { error: "Unapproved origin." }
-      return
-    end
-
     @game = Game.new(game_params)
     @game.username = @game.username.upcase
 
@@ -37,4 +26,11 @@ class Api::GamesController < ApplicationController
   def game_params
     params.require(:game).permit(:username, :score, :lines, :start_level, :end_level, :time)
   end
+
+  def approved_origin?
+    if request.headers['origin'] != 'http://golubitsky.github.io'
+      render json: { error: "Unapproved origin." }
+    end
+  end
+
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,12 +7,8 @@ class ApplicationController < ActionController::Base
   # For all responses in this controller, return the CORS access control headers.
 
   def cors_set_access_control_headers
-    puts "application controller"
-    puts request
     headers['Access-Control-Allow-Origin'] = 'http://golubitsky.github.io'
     headers['Access-Control-Allow-Methods'] = 'POST, GET'
-    # headers['Access-Control-Allow-Headers'] = '*'
     headers['Access-Control-Request-Method'] = 'POST'
-    # headers['Access-Control-Max-Age'] = "1728000"
   end
 end
